103e6948aef10ac0a504378c219a040aca102054
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-22 20:40:18 +0530

    feat: lean query in express

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [{ id: 1, username: "mahi", displayname: "Mahendran" }, { id: 2, username: "kumi", displayname: "Mahendran" }, { id: 3, username: "kai", displayname: "Mahendran" }, { id: 4, username: "pant", displayname: "Mahendran" }];
+
 app.get('/', (req, res) => {
     res.status(201).send({ msg: "Hello" });
 });
 
-app.get('/api/users', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.send([{ id: 1, username: "mahi", displayname: "Mahendran" }, { id: 2, username: "kumi", displayname: "Mahendran" }, { id: 3, username: "kai", displayname: "Mahendran" }, { id: 4, username: "pant", displayname: "Mahendran" }])
 })
 
-app.get('/api/products', (req, res) => {
-    res.send([{ id: 1, username: "mahi", displayname: "Mahendran" }, { id: 2, username: "kumi", displayname: "Mahendran" }, { id: 3, username: "kai", displayname: "Mahendran" }, { id: 4, username: "pant", displayname: "Mahendran" }])
+app.get('/api/users', (req, res) => {
+    console.log(req.query);
+    const { query: { filter, value } } = req;
+    if (!filter && !value) return res.send(mockUsers);
+    if (filter && value) return res.send(
+        mockUsers.filter((user) => {
+            return user[filter].includes(value)
+        })
+    );
+    // //not work
+    // if (filter && value) return res.send(
+    //     mockUsers.filter((user) => {
+    //         user[filter].includes(value) // need to retrun if we use {}
+    //     })
+    // );
 })
 
 app.listen(PORT, () => {
