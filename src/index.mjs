90182d31cfe85d5d86d9a12a3e1bf96eb4c3970d
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-04 19:56:36 +0530

    feat: learn delete and get using id in express

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 
 const mockUsers = [{ id: 1, username: "mahi", displayname: "Mahendran" }, { id: 2, username: "kumi", displayname: "Mahendran" }, { id: 3, username: "kai", displayname: "Mahendran" }, { id: 4, username: "pant", displayname: "Mahendran" }];
 
@@ -43,6 +47,15 @@ app.get('/api/users', (req, res) => {
     // );
 })
 
+app.get('/api/users/:id', (req, res) => {
+    let { params: { id } } = req;
+    id = parseInt(id);
+    if (isNaN(id)) return res.send(400);
+    const findUser = mockUsers.find((user) => user.id == id)
+    if (findUser == -1) return res.send(404);
+    return res.send(findUser);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is Running at ${PORT}`)
 })
@@ -73,4 +86,18 @@ app.patch('/api/users/:id', (req, res) => {
     if (findUserIndex === -1) return res.sendStatus(404);
     mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
     return res.sendStatus(200);
+})
+
+
+app.delete('/api/users/:id', (req, res) => {
+    let { params: { id } } = req;
+    id = parseInt(id);
+    if (isNaN(id)) return res.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex((user) => user.id == id);
+    if (findUserIndex != -1) {
+        mockUsers.splice(findUserIndex, 1);
+        return res.sendStatus(200);
+    } else {
+        return res.sendStatus(400);
+    }
 })
