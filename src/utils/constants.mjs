e3adab4837bf63000305ceada201a5d9bb1d5ccf
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-09 19:08:47 +0530

    feat: add pass in constant field

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -3,4 +3,4 @@ export const mockUsers = [
     { id: 2, username: "kumi", displayname: "Mahendran", password: "123456" },
     { id: 3, username: "kai", displayname: "Mahendran", password: "123456" },
     { id: 4, username: "pant", displayname: "Mahendran", password: "123456" },
-];
+]; 
