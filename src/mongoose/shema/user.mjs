fe9da8ac8a9b0ad03c7d3b5e1613ae00b5aebc03
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-15 17:51:14 +0530

    fix: typo

--- src/mongoose/shema/user.mjs
+++ src/mongoose/shema/user.mjs
@@ -1,18 +1,18 @@
-import mangoose from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mangoose.Schema({
     username: {
-        type: mangoose.Schema.Types.String,
+        type: mongoose.Schema.Types.String,
         required: true,
         unique: true,
     },
     displayName: {
-        type: mangoose.Schema.Types.String,
+        type: mongoose.Schema.Types.String,
     },
     password: {
-        type: mangoose.Schema.Types.String,
+        type: mongoose.Schema.Types.String,
         required: true,
     },
 })
 
-export const User = mangoose.model('User', userSchema)
+export const User = mongoose.model('User', userSchema)
